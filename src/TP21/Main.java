bc02e70f82e065a7ce177d051b60c979c274b116
Simon <xsub123@gmail.com>
2025-03-28 23:00:49 -0300

    Agregados metodos getRoot, isEmpty, getMaxElem

---
+++ src/TP21/Main.java
@@ -0,0 +1,12 @@
+package TP21;
+
+public class Main {
+    public static void main(String[] args) {
+        Tree miArbol = new Tree();
+
+
+
+
+    }
+
+}
--- src/TP21/Tree.java
+++ src/TP21/Tree.java
@@ -8,29 +8,69 @@ public class Tree {
         this.root = null;
     }
 
+
+    //El invocador solo pasa un numero, ya el metodo privado se encargara de crear los nodos correspondientes
     public void add(Integer value) {
         if (this.root == null)
-            this.root = new TreeNode(value);
+            this.root = new TreeNode(value);    //Si no hay raiz, el nuevo elemento lo es
         else
-            this.add(this.root,value);
+            this.add(this.root,value);     //sino, llama al metodo privado para ubicarlo donde corresponda
     }
 
+
+    //Encuentra la posicion indicada, crea un nodo con esa info y lo agrega al arbol
+    //Importante que sea privado, pues el invocador solo ingresa un numero, no conoce la
+    //existencia de los TreeNodes
     private void add(TreeNode actual, Integer value) {
+        //Si el nodo actual es mas grande, entonces el nuevo ira a la izquierda
         if (actual.getValue() > value) {
+            //Si la izquierda esta disponible, lo insertamos ahi
             if (actual.getLeft() == null) {
                 TreeNode temp = new TreeNode(value);
                 actual.setLeft(temp);
             } else {
+                //Sino, le preguntamos a dicho nodo izquierdo
                 add(actual.getLeft(),value);
             }
-        } else if (actual.getValue() < value) {
+        }
+        //Si el nodo actual es mas chico, el nuevo ira a la derecha
+        else if (actual.getValue() < value) {
+            //Si la derecha esta disponible, se inserta ahi
             if (actual.getRight() == null) {
                 TreeNode temp = new TreeNode(value);
                 actual.setRight(temp);
             } else {
+                //Sino, le preguntamos al nodo derecho
                 add(actual.getRight(),value);
             }
         }
+    }
+
+    //Devuelve la info de la raiz, si la misma existe
+    public Integer getRoot(){
+        return this.root!=null ? this.root.getValue() : null;
+    }
+
+    //Comprueba si el arbol esta vacio (no tiene raiz)
+    public boolean isEmpty(){
+        return this.root == null;
+    }
+
+
+    //Obtiene el mayor valor del arbol (el que esta mas a la derecha)
+    public Integer getMaxElem(){
+        if (this.isEmpty()){
+            return null;
+        }
+
+        //Partiendo de la raiz, si el nodo tiene un derecho, ese derecho se vuelve el de mayor valor
+        TreeNode actual = this.root;
+        while(actual.getRight() != null){
+            actual = actual.getRight();
+        }
+
+        //Una vez llegado al nodo derecho maximo, devuelvo la info
+        return actual.getValue();
     }
 
 }
