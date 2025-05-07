846a5e02ac4ef4e66684b738d6248747380de2e6
Simon Aceto <xsub123@gmail.com>
2025-05-07 19:39:09 -0300

    Agregado BFS

--- src/TP4/GrafoDirigido.java
+++ src/TP4/GrafoDirigido.java
@@ -202,6 +202,53 @@ public class GrafoDirigido<T> implements Grafo<T> {
         System.out.println("Finalizado el vertice: " + verticeId);
 
     }
+    
+    
+    public void BFS() {
+    	//Creamos la fila
+    	LinkedList<Integer> queue = new LinkedList();
+    	Set<Map.Entry<Integer, Character>> entries = this.estadoVertices.entrySet();
+    	
+    	//Marcamos todos los vertices como no visitados (Blanco)
+    	for (Map.Entry<Integer,Character> entry : entries) {
+    		entry.setValue('B');
+    	}
+    	
+    	//Si el vertice que recorremos esta sin visitar, lo visitamos
+    	//Pasamos la la queue que vamos a usar en todo el recorrido
+    	for (Map.Entry<Integer,Character> entry : entries) {
+    		if (entry.getValue().equals('B')) {
+    			BFS_Visit(entry.getKey(), queue);
+    		}
+    	}
+    	
+    }
+    
+    
+    
+    public void BFS_Visit(Integer verticeId, LinkedList<Integer> queue) {
+    	//Marcamos el actual como visitado
+    	this.estadoVertices.put(verticeId, 'N');
+    	
+    	//Agregamos el actual a la queue para empezar el recorrido
+    	queue.add(verticeId);
+    
+    	
+    	while (!queue.isEmpty()) {
+    		int actual = queue.poll(); //Como ya visitamos este, propagamos la busqueda a los adyacentes
+    		Iterator<Integer> adyacentes = obtenerAdyacentes(actual);
+    		while(adyacentes.hasNext()) {
+    			//Para cada adyacente blanco, repetimos el proceso. 
+    			//Lo marcamos, lo metemos a la queue y lo usamos para conseguir sus adyacentes
+    			int next = adyacentes.next();
+    			
+    			if(this.estadoVertices.get(next).equals('B')) {
+    				this.estadoVertices.put(next,'N');
+    				queue.add(next);
+    			}
+    		}
+    	}
+    }
 
 
 }
